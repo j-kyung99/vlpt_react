1d13663f2ee929c12df4cfbca84f101aea3a4d2a
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-03-03 15:31:25 +0900

    1-4
    JSX

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+    <Hello />
+    <div style={style}>{name}</div>
+    <div className="gray-box"></div>
+    </>
   );
 }
 
