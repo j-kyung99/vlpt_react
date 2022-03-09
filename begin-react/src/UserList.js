95ab482dd7b3c8e7398d45959b3212e93c1bb82a
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-03-09 13:46:06 +0900

    1-12
    useRef로 컴포넌트 안의 변수 만들기

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 
 function User({ user }){
     return (
@@ -8,24 +8,7 @@ function User({ user }){
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        },
-    ];
+function UserList({users}) {
     return (
         <div>
             {users.map(user => (
