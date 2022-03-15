1ad4103ad921d8fceb78698c3192fbd248a48462
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-03-15 22:02:24 +0900

    1-19
    React.memo를 사용한 컴포넌트 리렌더링 방지

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -15,26 +15,29 @@ function App() {
   const { username, email } = inputs;
   const onChange = useCallback( e => {
     const { name, value } = e.target;
-    setInputs({
+    setInputs(inputs => ({
       ...inputs,
       [name]: value
-    });
-  }, [inputs]);
+    }));
+  }, []);
   const [users, setUsers] = useState([
     {
         id: 1,
         username: 'velopert',
-        email: 'public.velopert@gmail.com'
+        email: 'public.velopert@gmail.com',
+        active: true
     },
     {
         id: 2,
         username: 'tester',
-        email: 'tester@example.com'
+        email: 'tester@example.com',
+        active: false
     },
     {
         id: 3,
         username: 'liz',
-        email: 'liz@example.com'
+        email: 'liz@example.com',
+        active: false
     },
 ]);
 const nextId = useRef(4);
@@ -51,17 +54,17 @@ const onCreate = useCallback(() => {
     email: ''
   });
   nextId.current += 1;
-}, [users, username, email]);
+}, [username, email]);
 
 const onRemove = useCallback( id => {
-  setUsers(users.filter(user => user.id !== id));
-}, [users]);
+  setUsers(users => users.filter(user => user.id !== id));
+}, []);
 const onToggle = useCallback( id => {
-  setUsers(
+  setUsers(users => 
     users.map(user =>
       user.id === id ? { ...user, active: !user.active} : user)
   );
-}, [users]);
+}, []);
 const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
